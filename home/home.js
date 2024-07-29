e69c072e2c2c7880281ce07b02fee4a4c0a089cb
aaronwu <aw80@rice.edu>
2024-07-29 14:05:04 -0500

    Fixed every aspect of the home page to look clean

--- home/home.js
+++ home/home.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const fadeInText = document.querySelector('.fade-in-left-letter-by-letter');
-    const text = fadeInText.innerText.replace('\n', ''); 
-    fadeInText.innerHTML = text.split('').map(letter => {
-        if (letter === ' ') return ' ';
-        return `<span>${letter}</span>`;
-    }).join('');
-    fadeInText.querySelectorAll('span').forEach((span, index) => {
-        span.style.animationDelay = `${index * 0.2}s`;
-    });
+    const fadeInTextH1 = document.querySelector('.intro h1.fade-in-left-letter-by-letter');
+    const fadeInTextH2 = document.querySelector('.intro h2.fade-in-left-letter-by-letter');
+    
+    function animateText(element) {
+        const text = element.innerText.replace('\n', '');
+        element.innerHTML = text.split('').map(letter => {
+            if (letter === ' ') return ' ';
+            return `<span>${letter}</span>`;
+        }).join('');
+        element.querySelectorAll('span').forEach((span, index) => {
+            span.style.animationDelay = `${index * 0.1}s`;
+        });
+    }
+
+    animateText(fadeInTextH1);
+    animateText(fadeInTextH2);
 });
