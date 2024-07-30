9703aa898db85b9bbf9a269646dddd6331aec543
aaronwu <aw80@rice.edu>
2024-07-30 18:58:34 -0500

    Created more specific UI elements to make the portfolio more interactive

--- home/home.js
+++ home/home.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const fadeInTextH1 = document.querySelector('.intro h1.fade-in-left-letter-by-letter');
     const fadeInTextH2 = document.querySelector('.intro h2.fade-in-left-letter-by-letter');
-    
+    const contactButton = document.querySelector('.contact-button.pop-in');
+
     function animateText(element) {
         const text = element.innerText.replace('\n', '');
         element.innerHTML = text.split('').map(letter => {
@@ -13,6 +14,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    function animateContactButton(element) {
+        element.style.animation = 'popIn 2s ease forwards';
+    }
+
     animateText(fadeInTextH1);
     animateText(fadeInTextH2);
+    animateContactButton(contactButton);
 });
